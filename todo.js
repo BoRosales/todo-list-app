108e1803642af6c5f4fab4582853e4966fe90a3e
Bo Rosales <bonirosales@gmail.com>
2020-03-04 15:34:28 -0600

    added some UI changes, added todo count and btn classes

--- todo.js
+++ todo.js
@@ -5,6 +5,7 @@ function getInputVal() {
     todoInput = document.getElementById("add-item").value.trim(); 
     if (todoInput.length < 4) {
         console.log("4 character minimum requirement")
+        return;
     } 
     console.log(todoInput);
     addToList(todoInput);
@@ -15,11 +16,13 @@ function addToList(input) {
     var listItem = document.createElement("li");
     listItem.appendChild(document.createTextNode(input));
     list.appendChild(listItem)
+    todoCount++;
     console.log(input);
 
-    var deleteBtn = document.createElement("button");
-    deleteBtn.appendChild(document.createTextNode("delete"));
-    listItem.appendChild(deleteBtn);
+    var deleteBtn = document.createElement("button"); // Creates the button element.
+    deleteBtn.classList.add("deleteTask" + todoCount); // This adds the class "deleteTask" and todoCount.
+    deleteBtn.appendChild(document.createTextNode("delete")); 
+    listItem.appendChild(deleteBtn); // append the deleteBtn to list item.
 }
 
 // TODO: Update counter on UI, that will say what todo task you are on
@@ -29,5 +32,10 @@ function updateCounter(todoCount) {
 
 // TODO: Delete button next to task. 
 function deleteTask() {
+    
+}
 
+// TODO: Show error area if there is error. 
+function errorArea() {
+    
 }
